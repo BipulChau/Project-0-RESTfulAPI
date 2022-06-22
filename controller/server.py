164502c6fae92f3b87915698f05a1945eb05f02a
Bipul Chaudhary <bipulchau@gmail.com>
2022-06-21 21:23:51 -0400

    set GET, POST and PUT methods for route customers

--- controller/server.py
+++ controller/server.py
@@ -7,9 +7,13 @@ app = Flask(__name__)
 @app.route('/home')
 @app.route('/index')
 def home():
-    return "Welcome to MyBank : Fast, Friendly & Accurate Service"
+    return "Welcome to MyBank : Fast, Friendly & Accurate Service", 200
+
+
+@app.route('/api/customers/', methods=['GET', 'POST', 'PUT'])
+def customers():
+    return {}, 200
 
 
 if __name__ == "__main__":
     app.run(debug=True, port=3132)
-
