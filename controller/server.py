abd32afa4cf374eca22ae27d5c50b7cf47c80a99
Bipul Chaudhary <bipulchau@gmail.com>
2022-06-21 21:37:26 -0400

    Dynamic Route. Search customer details based on name

--- controller/server.py
+++ controller/server.py
@@ -10,9 +10,14 @@ def home():
     return "Welcome to MyBank : Fast, Friendly & Accurate Service", 200
 
 
-@app.route('/api/customers/', methods=['GET', 'POST', 'PUT'])
+@app.route('/customers/', methods=['GET', 'POST', 'PUT'])
 def customers():
-    return {}, 200
+    return "Details of all customers appears here", 200
+
+
+@app.route('/customer/<customer_name>')
+def profile(customer_name):
+    return "Profile of individual customer appears here", 200
 
 
 if __name__ == "__main__":
